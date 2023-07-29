f70b1d17a080c03049afd3b92ce115ee22a6380a
Ibrahim Abdulhaqq <enessyibrahim@gmail.com>
2023-07-29 10:03:23 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDkNsAdr7gMs8LfKAQDgsmBiOTmbFpVjNg",
-  authDomain: "attendies-2efdd.firebaseapp.com",
-  projectId: "attendies-2efdd",
-  storageBucket: "attendies-2efdd.appspot.com",
-  messagingSenderId: "443030887286",
-  appId: "1:443030887286:web:8da78da8c6c09751927857",
+   apiKey: "AIzaSyDd1AbTTffO6MQFGm49bmEfw-j8UX5LPJM",
+  authDomain: "attendance-c5a47.firebaseapp.com",
+  projectId: "attendance-c5a47",
+  storageBucket: "attendance-c5a47.appspot.com",
+  messagingSenderId: "4342999055",
+  appId: "1:4342999055:web:ef7152d30ceb4d20359b7a",
   measurementId: "G-WECXEHSGFD"
 };
 
